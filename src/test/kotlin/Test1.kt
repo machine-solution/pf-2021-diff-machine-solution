1a96817c735da3474c8dcfe90e42571fcd79f0e1
Andrey Zaitsev <machine.solution353@gmail.com>
2021-09-03 21:18:18 +0300

    Released reconstruction a subsequence in function "longestCommonSubsequence" (not tested)

--- src/test/kotlin/Test1.kt
+++ src/test/kotlin/Test1.kt
@@ -3,12 +3,15 @@ import kotlin.test.*
 internal class Test1 {
 
     @Test
-    fun longestCommonSubsequenceLenthTest() {
-        assertEquals(0, longestCommonSubsequenceLenth(arrayOf("A", "B", "C"), arrayOf("ABC", "D", "E")))
-        assertEquals(2, longestCommonSubsequenceLenth(arrayOf("A", "b", "C"), arrayOf("A", "B", "C")))
-        assertEquals(1, longestCommonSubsequenceLenth(arrayOf("A B", "C"), arrayOf("A", "B", "E", "C")))
-        assertEquals(4, longestCommonSubsequenceLenth(arrayOf("A", "B", "C", "D", "E", "F"), arrayOf("B", "D", "C", "A", "E", "F")))
-        assertEquals(3, longestCommonSubsequenceLenth(arrayOf("B", "A", "N", "A", "N", "A"), arrayOf("A", "T", "A", "N")))
-        assertEquals(0, longestCommonSubsequenceLenth(arrayOf("B", "A", "N", "A", "N", "A"), arrayOf()))
+    fun longestCommonSubsequenceLengthTest() {
+        assertEquals(0, longestCommonSubsequenceLength(arrayOf("A", "B", "C"), arrayOf("ABC", "D", "E")))
+        assertEquals(2, longestCommonSubsequenceLength(arrayOf("A", "b", "C"), arrayOf("A", "B", "C")))
+        assertEquals(1, longestCommonSubsequenceLength(arrayOf("A B", "C"), arrayOf("A", "B", "E", "C")))
+        assertEquals(2, longestCommonSubsequenceLength(arrayOf("A B", "C"), arrayOf("A", "A B", "E", "C")))
+        assertEquals(4, longestCommonSubsequenceLength(arrayOf("A", "B", "C", "D", "E", "F"),
+            arrayOf("B", "D", "C", "A", "E", "F")))
+        assertEquals(3, longestCommonSubsequenceLength(arrayOf("B", "A", "N", "A", "N", "A"),
+            arrayOf("A", "T", "A", "N")))
+        assertEquals(0, longestCommonSubsequenceLength(arrayOf("B", "A", "N", "A", "N", "A"), arrayOf()))
     }
 }
