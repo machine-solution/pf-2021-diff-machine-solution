853009235dbfbd5ddbae6c89e920b3e8cf0f93fe
Andrey Zaitsev <machine.solution353@gmail.com>
2021-09-18 00:08:13 +0300

    Revert "Successful tested a longestCommonSubsequence"
    This reverts commit b27809864aebf97b0991c8028bd45b7e832b153e.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -33,11 +33,8 @@ fun longestCommonSubsequence(sequence1: Array<String>, sequence2: Array<String>)
             it1--
             it2--
         }
-        else if (length[it1 - 1][it2] > length[it1][it2 - 1]) {
+        else{
             it1--
-        }
-        else {
-            it2--
         }
     }
 
--- src/test/kotlin/Test1.kt
+++ src/test/kotlin/Test1.kt
@@ -19,8 +19,8 @@ internal class Test1 {
         assertEquals(0, longestCommonSubsequenceLength(arrayOf("B", "A", "N", "A", "N", "A"), arrayOf()))
     }
 
-    // if answer is a common subsequence, return answer.size, else return -1
-    private fun longestCommonSubsequenceChecker(sequence1: Array<String>, sequence2: Array<String>, answer: Array<String>): Int {
+    // if answer is a common subsequence, return answer.size, else return 0
+    fun longestCommonSubsequenceChecker(sequence1: Array<String>, sequence2: Array<String>, answer: Array<String>): Int {
         fun isSubsequence(subsequence: Array<String>, sequence: Array<String>): Boolean {
             var it1: Int = 0
             for (it2 in sequence.indices) {
@@ -36,25 +36,14 @@ internal class Test1 {
         return if (isSubsequence(answer, sequence1) && isSubsequence(answer, sequence2)) {
            answer.size
         } else {
-            -1
+            0
         }
     }
 
     @Test
     fun longestCommonSubsequenceTest() {
-        fun checker(expected: Int, sequence1: Array<String>, sequence2: Array<String>) {
-            assertEquals(expected, longestCommonSubsequenceChecker(sequence1, sequence2,
-                longestCommonSubsequence(sequence1, sequence2)))
-        }
+        assertEquals(0, longestCommonSubsequenceChecker(arrayOf("A", "B", "C"), arrayOf("ABC", "D", "E"),
+            longestCommonSubsequence(arrayOf("A", "B", "C"), arrayOf("ABC", "D", "E"))))
 
-        checker(0, arrayOf("A", "B", "C"), arrayOf("ABC", "D", "E"))
-        checker(2, arrayOf("A", "b", "C"), arrayOf("A", "B", "C"))
-        checker(1, arrayOf("A B", "C"), arrayOf("A", "B", "E", "C"))
-        checker(2, arrayOf("A B", "C"), arrayOf("A", "A B", "E", "C"))
-        checker(4, arrayOf("A", "B", "C", "D", "E", "F"), arrayOf("B", "D", "C", "A", "E", "F"))
-        checker(3, arrayOf("B", "A", "N", "A", "N", "A"), arrayOf("A", "T", "A", "N"))
-        checker(0, arrayOf("B", "A", "N", "A", "N", "A"), arrayOf())
-        checker(3, arrayOf("A", " A", "B", "A ", "B.", "A"), arrayOf("A", "A", "B", "A"))
-        checker(4, arrayOf("A", " A", "B", "A ", "B.", "A"), arrayOf("A", " A", "B", "A"))
     }
 }
