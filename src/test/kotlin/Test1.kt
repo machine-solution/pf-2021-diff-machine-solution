9a7f14d0ef74609c84fccb5a0a59204242942976
Andrey Zaitsev <machine.solution353@gmail.com>
2021-09-18 00:08:15 +0300

    Revert "Realized a checker for longestCommonSubsequence"
    This reverts commit 7d0b13e9b4ba6cfde4cc84b1f3b6668c90d6a567.

--- src/test/kotlin/Test1.kt
+++ src/test/kotlin/Test1.kt
@@ -1,9 +1,5 @@
 import kotlin.test.*
 
-
-
-
-
 internal class Test1 {
 
     @Test
@@ -17,33 +13,5 @@ internal class Test1 {
         assertEquals(3, longestCommonSubsequenceLength(arrayOf("B", "A", "N", "A", "N", "A"),
             arrayOf("A", "T", "A", "N")))
         assertEquals(0, longestCommonSubsequenceLength(arrayOf("B", "A", "N", "A", "N", "A"), arrayOf()))
-    }
-
-    // if answer is a common subsequence, return answer.size, else return 0
-    fun longestCommonSubsequenceChecker(sequence1: Array<String>, sequence2: Array<String>, answer: Array<String>): Int {
-        fun isSubsequence(subsequence: Array<String>, sequence: Array<String>): Boolean {
-            var it1: Int = 0
-            for (it2 in sequence.indices) {
-                if (it1 == subsequence.size)
-                    return true
-                if (sequence[it2] == subsequence[it1]) {
-                    it1++;
-                }
-            }
-            return it1 == subsequence.size
-        }
-
-        return if (isSubsequence(answer, sequence1) && isSubsequence(answer, sequence2)) {
-           answer.size
-        } else {
-            0
-        }
-    }
-
-    @Test
-    fun longestCommonSubsequenceTest() {
-        assertEquals(0, longestCommonSubsequenceChecker(arrayOf("A", "B", "C"), arrayOf("ABC", "D", "E"),
-            longestCommonSubsequence(arrayOf("A", "B", "C"), arrayOf("ABC", "D", "E"))))
-
     }
 }
