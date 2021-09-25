58af677d86578637480ebdf06dfdaf405583911d
Andrey Zaitsev <machine.solution353@gmail.com>
2021-09-25 18:55:59 +0300

    Add colourful output

--- src/main/kotlin/input_output.kt
+++ src/main/kotlin/input_output.kt
@@ -1,5 +1,23 @@
 import java.io.File
 
+// Цветной вывод
+object Color {
+    const val ANSI_RESET = "\u001B[0m"
+    const val ANSI_RED = "\u001B[31m"
+    const val ANSI_GREEN = "\u001B[32m"
+    const val ANSI_BLUE = "\u001B[34m"
+}
+
+fun printlnRed(string: String) {
+    println(Color.ANSI_RED + string + Color.ANSI_RESET)
+}
+fun printlnGreen(string: String) {
+    println(Color.ANSI_GREEN + string + Color.ANSI_RESET)
+}
+fun printlnBlue(string: String) {
+    println(Color.ANSI_BLUE + string + Color.ANSI_RESET)
+}
+
 // Возвращает содержимое файла filename, разделённое на строки
 fun readFileLines(filename: String): List<String> {
         return File(filename).readLines()
@@ -36,12 +54,17 @@ fun writeScreenLines(lines: List<DiffLineBlock>) {
         // Вывод заголовка блока
         val ind = if (block.first != block.last) "${block.first}-${block.last}" else "${block.first}"
         if (block.add)
-            print("\n@ a $ind @")
+            printlnBlue("\n@ a $ind @")
         else
-            print("\n@ d $ind @")
+            printlnBlue("\n@ d $ind @")
         // Вывод содержимого блока
-        for (str in block.strings)
-            print("\n> $str")
+        if (block.add) {
+            for (str in block.strings)
+                printlnGreen("> $str")
+        } else {
+            for (str in block.strings)
+                printlnRed("> $str")
+        }
     }
     for (block in lines)
         writeDiffLineBlock(block)
@@ -52,11 +75,8 @@ fun getCorrectPath(fileAlias: String): String {
     println("Enter the path of $fileAlias")
     var path = readLine()
     var file = File(path)
-    while (file == null || !file.isFile) {
-        if (file == null)
-            println("The path of file is incorrect. Please, try again.")
-        else
-            println("Unable to convert file to text. Please, the path of another file.")
+    while (!file.isFile) {
+        println("Unable to convert file to text. Please, the path of another file.")
         path = readLine()
         file = File(path)
     }
